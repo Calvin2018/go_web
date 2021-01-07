68b745d37f1d31310a6b41ff945a08984bf18423
Calvin <guojian@Calvin-KuodeMacBook-Pro.local>
2021-01-07 15:16:17 +0800

    third commit

--- lesson27/bubble/main.go
+++ lesson27/bubble/main.go
@@ -7,6 +7,7 @@ import (
 )
 
 func main() {
+	//have fun with golang!
 	// 创建数据库
 	// sql: CREATE DATABASE bubble;
 	// 连接数据库
@@ -14,7 +15,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	defer dao.Close()  // 程序退出关闭数据库连接
+	defer dao.Close() // 程序退出关闭数据库连接
 	// 模型绑定
 	dao.DB.AutoMigrate(&models.Todo{})
 	// 注册路由
